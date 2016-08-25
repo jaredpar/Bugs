1bec1fb0f69166538fed72a3e4cc21398f1c09dc
Jared Parsons <jaredpar@microsoft.com>
2016-08-25 10:50:29 -0700

    Simple data dump

---
+++ DumpBugs/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+using System.IO;
+using System.Collections.Immutable;
+using FileMode = System.IO.FileMode;
+
+namespace DumpBugs
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            return Go().Result;
+        }
+
+        private static async Task<int> Go()
+        {
+            try
+            {
+                var client = new GitHubClient(new ProductHeaderValue("JaredsAmazingGithubBugClient"));
+                client.Credentials = ReadCredentials();
+                var queryUtil = new QueryUtil(client);
+                var repo = await client.Repository.Get("dotnet", "roslyn");
+                var issueQuery = GetIssueQuery();
+                var list = await queryUtil.GetIssues(repo, issueQuery);
+                var filtered = list.Where(x => x.Labels.Any(l => l.Name == "Bug" || l.Name == "Feature Request"));
+
+                using (var stream = File.Open(@"c:\users\jaredpar\issues.csv", FileMode.Create))
+                using (var textWriter = new StreamWriter(stream))
+                {
+                    foreach (var issue in filtered)
+                    {
+                        textWriter.WriteLine($"{issue.Milestone.Title},{issue.Assignee.Login},{issue.Url}");
+                    }
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"{ex.Message}");
+                return 1;
+            }
+        }
+
+        private static Credentials ReadCredentials()
+        {
+            var text = File.ReadAllText(@"c:\users\jaredpar\github.txt").Trim();
+            var items = text.Split(':');
+            return new Credentials(items[0], items[1]);
+        }
+
+        private static IssueQuery GetIssueQuery()
+        {
+            var team = new[]
+            {
+                "jaredpar",
+                "gafter",
+                "VSadov",
+                "cston",
+                "tyoverby",
+                "jcouv",
+                "AlekseyTs",
+                "agocke",
+            };
+
+            var milestones = new[]
+            {
+                "2.0 (Preview 5)",
+                "2.0 (RC)",
+                "2.0 (RTM)",
+            };
+
+            return new IssueQuery(team, milestones);
+        }
+    }
+
+    internal class IssueQuery
+    {
+        internal ImmutableArray<string> Team { get; }
+        internal ImmutableArray<string> Milestones { get; }
+
+        internal IssueQuery(IEnumerable<string> team, IEnumerable<string> milestones)
+        {
+            Team = team.ToImmutableArray();
+            Milestones = milestones.ToImmutableArray();
+        }
+    }
+}
---
+++ DumpBugs/QueryUtil.cs
@@ -0,0 +1,86 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumpBugs
+{
+    internal sealed class QueryUtil
+    {
+        internal const int DefaultPageSize = 50;
+
+        private readonly GitHubClient _client;
+
+        internal QueryUtil(GitHubClient client)
+        {
+            _client = client;
+        }
+
+        internal async Task<List<Issue>> GetIssues(Repository repo, IssueQuery query)
+        {
+            // TODO: need to handle unassigned bugs too. 
+
+            var list = new List<Issue>();
+            foreach (var member in query.Team)
+            {
+                foreach (var title in query.Milestones)
+                {
+                    var milestone = await GetMilestone(repo, title);
+                    var request = new RepositoryIssueRequest();
+                    request.Milestone = $"{milestone.Number}";
+                    request.Assignee = member;
+                    request.State = ItemStateFilter.Open;
+                    list.AddRange(await GetIssues(repo, request));
+                }
+            }
+
+            return list;
+        }
+
+        internal async Task<Milestone> GetMilestone(Repository repo, string title)
+        {
+            var all = await _client.Issue.Milestone.GetAllForRepository(repo.Id);
+            var comparer = StringComparer.OrdinalIgnoreCase;
+            foreach (var item in all)
+            {
+                if (comparer.Equals(item.Title, title))
+                {
+                    return item;
+                }
+            }
+
+            throw new Exception($"Unable to find milestone with title {title}");
+        }
+
+        internal async Task<List<Issue>> GetIssues(Repository repo, RepositoryIssueRequest request, int pageSize = DefaultPageSize)
+        {
+            var list = new List<Issue>();
+            var options = new ApiOptions()
+            {
+                PageCount = 1,
+                StartPage = 0,
+                PageSize = pageSize
+            };
+
+            var done = false;
+            var issues = _client.Issue;
+            do
+            {
+                var pageIssues = await issues.GetAllForRepository(repo.Id, request, options);
+                list.AddRange(pageIssues);
+                if (pageIssues.Count < pageSize)
+                {
+                    done = true;
+                }
+                else
+                {
+                    options.StartPage++;
+                }
+            } while (!done);
+
+            return list;
+        }
+    }
+}
