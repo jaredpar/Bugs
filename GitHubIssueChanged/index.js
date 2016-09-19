956fcef128a54a4c3ef8aca2aba3279a0db5aea1
Jared Parsons <jaredpparsons@gmail.com>
2016-09-19 10:16:05 -0700

    Updated a comment string to test CD

--- GitHubIssueChanged/index.js
+++ GitHubIssueChanged/index.js
@@ -1,4 +1,3 @@
-// Please visit http://go.microsoft.com/fwlink/?LinkID=761099&clcid=0x409 for more information on settting up Github Webhooks
 module.exports = function (context, data) {
     context.log('Issue: ', data.issue.number);
     
